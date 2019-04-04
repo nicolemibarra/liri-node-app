b333478c48a430d854335c2d396f08de3390dcf4
nicolemibarra <nicolemibarra@icloud.com>
2019-04-03 19:52:43 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -4,3 +4,13 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
+node liri.js concert-this <artist/band name here>
+  
+"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+Name of the venue
+Venue location
+Date of the Event (use moment to format this as "MM/DD/YYYY"
+                   
+node liri.js spotify-this-song '<song name here>'
+                   
+                   

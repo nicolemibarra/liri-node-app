1d66dd7f259cf52ce7081f95d6b33cb83273f45d
nicolemibarra <nicolemibarra@icloud.com>
2019-04-06 18:36:52 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
 console.log('this is loaded');
 
-exports.spotify = {
+module.exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+module.exports.omdb = {
+  key: process.env.OMDB_KEY
 };
